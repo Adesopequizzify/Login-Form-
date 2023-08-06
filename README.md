4653771b69be5ae8319ebc3e145aa8c4341cc912
adesope@quizzify.com.ng <adesope@quizzify.com.ng>
2023-08-06 20:01:28 +0100

    Sucess

---
+++ README.md
@@ -0,0 +1,38 @@
+# User-Friendly Login Form with Password Strength and Error Handling
+
+This repository contains a simple web-based login form with password strength indication and error handling. The project is built using HTML, CSS, and JavaScript to create an interactive and user-friendly login experience.
+
+## Features
+
+- User-friendly login form with username (email) and password fields.
+- Real-time password strength indication using color-coded feedback.
+- AJAX-based form submission for seamless user experience.
+- Loading spinner to provide visual feedback during form submission.
+- Error handling and display of error messages in a modal pop-up.
+
+## How to Use
+
+1. Clone the repository: `git clone https://github.com/Adesopequizzify/Login-Form-.git`
+2. Open `index.html` in your web browser to see the login form.
+3. Enter a username (email) and password to test the form.
+
+## Files and Structure
+
+- `index.html`: The main HTML file containing the login form and necessary elements.
+- `styles.css`: Cascading Style Sheets (CSS) for styling the form and layout.
+- `script.js`: JavaScript code for password strength indication, form submission, and error handling.
+- `login.php`: PHP script (to be created) for processing form submissions on the server.
+
+## Customization
+
+You can customize the appearance and behavior of the form by modifying the CSS and JavaScript files. Additionally, you can create a `login.php` script to handle form submissions on the server-side and interact with a database.
+
+Feel free to expand upon this project to add more features, security measures, or integrate it with your existing applications.
+
+## Credits
+
+This project was created based on the guidance and assistance provided by OpenAI's GPT-3 model and TechQ Development Team.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
---
+++ script.js
@@ -0,0 +1,68 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const passwordInput = document.getElementById("password");
+    const passwordStrength = document.getElementById("password-strength");
+    const loadingOverlay = document.getElementById("loading-overlay");
+    const errorModal = document.getElementById("error-modal");
+    const closeModalButton = document.getElementById("close-error-modal");
+    const errorMessage = document.getElementById("error-message");
+
+    passwordInput.addEventListener("input", function () {
+        const password = passwordInput.value;
+        const strength = calculatePasswordStrength(password);
+        updatePasswordStrengthDisplay(strength);
+    });
+
+    closeModalButton.addEventListener("click", function () {
+        errorModal.style.display = "none";
+    });
+
+    const form = document.querySelector("form");
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        loadingOverlay.style.display = "flex";
+        errorModal.style.display = "none";
+
+        const formData = new FormData(form);
+        const xhr = new XMLHttpRequest();
+        xhr.open("POST", "login.php", true);
+
+        xhr.onload = function () {
+            loadingOverlay.style.display = "none";
+
+            if (xhr.status >= 200 && xhr.status < 400) {
+                const response = JSON.parse(xhr.responseText);
+                if (response.success) {
+                    window.location.href = "login-success.php";
+                } else {
+                    errorMessage.textContent = response.message;
+                    errorModal.style.display = "flex";
+                }
+            } else {
+                errorMessage.textContent = "An error occurred. Please try again later.";
+                errorModal.style.display = "flex";
+            }
+        };
+
+        xhr.onerror = function () {
+            loadingOverlay.style.display = "none";
+            errorMessage.textContent = "An error occurred. Please try again later.";
+            errorModal.style.display = "flex";
+        };
+
+        xhr.send(formData);
+    });
+
+    function calculatePasswordStrength(password) {
+        if (password.length >= 8) {
+            return "strong";
+        } else {
+            return "weak";
+        }
+    }
+
+    function updatePasswordStrengthDisplay(strength) {
+        passwordStrength.textContent = strength.charAt(0).toUpperCase() + strength.slice(1);
+        passwordStrength.style.color = strength === "strong" ? "#2ecc71" : "#e74c3c";
+    }
+});
